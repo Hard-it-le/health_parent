d9098c9f7af515c364e5f3f06d3ba062a5105f44
yu183 <yjl>
2020-11-30 15:00:26 +0800

    传智健康管理系统

--- health_common/src/main/java/com/yjl/utils/SMSUtils.java
+++ health_common/src/main/java/com/yjl/utils/SMSUtils.java
@@ -30,8 +30,8 @@ public class SMSUtils {
 		final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
 		final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 		// 替换成你的AK
-		final String accessKeyId = "LTAI4GBbG6DdXtk4CXsTvgJk";// 你的accessKeyId,参考本文档步骤2
-		final String accessKeySecret = "J8NJYfbJK0SitkfkaEO9tg0Ra0Km1e";// 你的accessKeySecret，参考本文档步骤2
+		final String accessKeyId = "LTAI4GBbG6DdXtk4CXsTvgJk1";// 你的accessKeyId,参考本文档步骤2
+		final String accessKeySecret = "J8NJYfbJK0SitkfkaEO9tg0Ra0Km1e1";// 你的accessKeySecret，参考本文档步骤2
 		// 初始化ascClient,暂时不支持多region（请勿修改）
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
